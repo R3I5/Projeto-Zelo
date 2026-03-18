77eca216c9a4e52779ac67c8bd0368d558062797
João Reis <j.victor.reis20044@gmail.com>
2026-03-17 21:24:53 -0300

    Testando sensor RTC

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
-#include <Arduino.h>
+/*******************************************************************************
+* Real Time Clock com Pinos Customizados no ESP32
+*******************************************************************************/
+  
+//Inclui as bibliotecas
+#include <Wire.h>
+#include "RTClib.h"
 
-// put function declarations here:
-int myFunction(int, int);
+RTC_DS1307 rtc; //Objeto rtc da classe DS1307
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Definindo nossos pinos customizados
+#define PINO_SDA 27
+#define PINO_SCL 14
+  
+char diasDaSemana[7][12] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado"}; //Dias da semana
+
+void setup () {
+  Serial.begin(115200);                        // Ajustado para o padrão do ESP32
+  delay(2000);                                 // Tempo para o monitor serial do Ubuntu conectar
+  
+  Serial.println("Iniciando o sistema...");
+  
+  // INICIA O I2C NOS PINOS ESCOLHIDOS ANTES DE CHAMAR O RTC
+  Wire.begin(PINO_SDA, PINO_SCL); 
+
+  if (! rtc.begin()) {                         
+    Serial.println("RTC NAO INICIALIZADO - Verifique as conexoes!");    
+    while (1);                                 
+  }
+  
+  rtc.adjust(DateTime(2026, 3, 17, 20, 58, 00)); 
+  
+  Serial.println("RTC conectado ");
+  delay(100);                                  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop () {
+  DateTime agora = rtc.now();                             
+  Serial.print("Data: ");
+  Serial.print(agora.day(), DEC);                         
+  Serial.print('/');                                      
+  Serial.print(agora.month(), DEC);                       
+  Serial.print('/');                                      
+  Serial.print(agora.year(), DEC);                        
+  Serial.print(" / Dia da semana: ");                     
+  Serial.print(diasDaSemana[agora.dayOfTheWeek()]);       
+  Serial.print(" / Horas: ");                             
+  Serial.print(agora.hour(), DEC);                        
+  Serial.print(':');                                      
+  Serial.print(agora.minute(), DEC);                      
+  Serial.print(':');                                      
+  Serial.print(agora.second(), DEC);                      
+  Serial.println();                                       
+  delay(1000);                                            
 }
